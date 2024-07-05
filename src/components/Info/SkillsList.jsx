729e848ab97d1964d3ecaeb7c607d3a5402591ed
Raphoto1 <rrhhmmtt@gmail.com>
2024-07-05 18:58:58 -0500

    skillset descktop set missing mobile

--- src/components/Info/SkillsList.jsx
+++ src/components/Info/SkillsList.jsx
@@ -1,18 +1,59 @@
+//app imports
 import React from "react";
 import { v4 as uuidv4 } from "uuid";
 import { Box, CircularProgress, Image } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+
 export default function SkillsList({ data }) {
   return (
     <>
-      <Box display={"flex"} pb={5} justifyContent={"center"} flexWrap={"wrap"} w={"fit-content"}>
+      <Box
+        display={"flex"}
+        justifyContent={"center"}
+        flexWrap={"wrap"}
+        w={"fit-content"}
+      >
         {data.map((itm) => (
-          <Box maxW={'20vw'} p={'4vw'} key={uuidv4()} display={'flex'} justifyContent={'center'} alignContent={'center'} alignItems={'center'}>
-            <CircularProgress size={'10vw'} value={itm.strenght} zIndex={2} position={'absolute'}/>
-            <Box w={'5vw'} display={'flex'} position={"absolute"} justifyContent={'center'} zIndex={1}>
-              <Image src='\img\icons\soft\html-5.png' objectFit={"fill"} alt="Rafa's" />
+          <Box
+            p={"4vw"}
+            w={'33%'}
+            h={'100%'}
+            key={uuidv4()}
+            display={"flex"}
+            flexDir={'column'}
+            justifyContent={"center"}
+            alignItems={"center"}
+            backgroundColor={'black'}
+            borderColor={'white'}
+            borderWidth={5}
+            p={2}
+          >
+            <Box display={'flex'} justifyContent={'center'} alignContent={'center'} alignItems={'center'}>
+              <CircularProgress
+                size={"10vw"}
+                value={itm.strenght}
+                zIndex={2}
+              />
+              <motion.div
+              whileHover={{scale:1.5}}
+              className="flex absolute justify-center md:w-20 sm:w-1"
+                zIndex={1}
+              >
+                <Image
+                  src={`${itm.image}`}
+                  objectFit={"fill"}
+                  alt={`${itm.name}`}
+                />
+              </motion.div>
             </Box>
-            <Box zIndex={3} display={'flex'} justifyContent={'end'} justifyItems={'end'} justifySelf={'end'}>
-                <h3 className='text-xl font-medium'>{itm.name}</h3>
+            <Box
+              zIndex={3}
+              display={"flex"}
+              backgroundColor={'black'}
+              alignContent={'end'}
+              alignItems={'end'}
+            >
+              <h3 className="text-xl font-medium">{itm.name}</h3>
             </Box>
           </Box>
         ))}
--- src/components/Info/SkillsetDev.jsx
+++ src/components/Info/SkillsetDev.jsx
@@ -6,28 +6,40 @@ import TextAnimSubt from "../TextAnim/TextAnimSubt";
 import SkillsList from "./SkillsList";
 export default function SkillsetDev() {
   const frontEnd = [
-    { name: "TEch", strenght: 20, image: "path" },
-    { name: "TEch2", strenght: 30, image: "path" },
-    { name: "TEch3", strenght: 40, image: "path" },
-    { name: "TEch3", strenght: 40, image: "path" },
+    { name: "TEch", strenght: 20, image: "img/icons/soft/html-5.png" },
+    { name: "TEch2", strenght: 30, image: "img/icons/soft/html-5.png" },
+    { name: "TEch3", strenght: 90, image: "img/icons/soft/html-5.png" },
+    { name: "TEch3", strenght: 40, image: "img/icons/soft/html-5.png" },
   ];
 
   return (
     <>
       <Box display={"flex"} flexDirection={"column"}>
-        <Box display={"flex"} w={"100vw"} justifyContent={"center"} justifyItems={"center"} justifySelf={"center"} pb={10}>
+        <Box
+          display={"flex"}
+          w={"100vw"}
+          justifyContent={"center"}
+          justifyItems={"center"}
+          justifySelf={"center"}
+          pb={10}
+        >
           <TextAnimBold text={`Skillset`} />
         </Box>
-        <Box display={"flex"} w={"100vw"} flexFlow={"wrap"} justifyContent={"space-evenly"}>
-          <Box backgroundColor={"blue"} className='md:w-1/3 text-center'>
+        <Box
+          display={"flex"}
+          w={"100vw"}
+          flexFlow={"wrap"}
+          justifyContent={"space-evenly"}
+        >
+          <Box backgroundColor={"blue"} className="md:w-1/3 text-center">
             <TextAnimSubt text={"Front End"} />
             <SkillsList data={frontEnd} />
           </Box>
-          <Box backgroundColor={"green"} className='md:w-1/3 text-center'>
+          <Box backgroundColor={"green"} className="md:w-1/3 text-center">
             <TextAnimSubt text={"Back End"} />
             <SkillsList data={frontEnd} />
           </Box>
-          <Box backgroundColor={"red"} className='md:w-1/3 text-center'>
+          <Box backgroundColor={"red"} className="md:w-1/3 text-center">
             <TextAnimSubt text={"General"} />
             <SkillsList data={frontEnd} />
           </Box>
